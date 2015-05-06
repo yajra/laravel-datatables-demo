c94a1830ae9f3715e2c43c42df044e66e7718a56
Arjay Angeles <aqangeles@gmail.com>
2015-05-06 20:36:02 +0800

    compressed sidebar to fit more menus

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,36 +1,36 @@
-<div class="list-group">
-	<a href="#" class="list-group-item disabled">Eloquent ORM</a>
-	<a href="{!! url('eloquent/basic') !!}" class="list-group-item {!! Request::is('eloquent/basic') ? 'active' : '' !!}">Basic</a>
-	<a href="{!! url('eloquent/basic-object') !!}" class="list-group-item {!! Request::is('eloquent/basic-object') ? 'active' : '' !!}">Object Data Source</a>
-    <a href="{!! url('eloquent/multi-filter-select') !!}" class="list-group-item {!! Request::is('eloquent/multi-filter-select') ? 'active' : '' !!}">Column Search</a>
-	<a href="{!! url('eloquent/add-edit-remove-column') !!}" class="list-group-item {!! Request::is('eloquent/add-edit-remove-column') ? 'active' : '' !!}">Add/Edit/Remove Column</a>
-	<a href="{!! url('eloquent/dt-row') !!}" class="list-group-item {!! Request::is('eloquent/dt-row') ? 'active' : '' !!}">DT Row Option</a>
-	<a href="{!! url('eloquent/custom-filter') !!}" class="list-group-item {!! Request::is('eloquent/custom-filter') ? 'active' : '' !!}">Custom Filter</a>
-	<a href="{!! url('eloquent/carbon') !!}" class="list-group-item {!! Request::is('eloquent/carbon') ? 'active' : '' !!}">DateTime/Carbon Objects</a>
-	<a href="{!! url('eloquent/relationships') !!}" class="list-group-item {!! Request::is('eloquent/relationships') ? 'active' : '' !!}">Eager Loading</a>
-	<a href="{!! url('eloquent/joins') !!}" class="list-group-item {!! Request::is('eloquent/joins') ? 'active' : '' !!}">Join Queries</a>
-</div>
+<ul class="nav navbar-list">
+	<li class="header">Eloquent ORM</li>
+	<li><a href="{!! url('eloquent/basic') !!}" class="{!! Request::is('eloquent/basic') ? 'active' : '' !!}">Basic</a></li>
+	<li><a href="{!! url('eloquent/basic-object') !!}" class="{!! Request::is('eloquent/basic-object') ? 'active' : '' !!}">Object Data Source</a></li>
+    <li><a href="{!! url('eloquent/multi-filter-select') !!}" class="{!! Request::is('eloquent/multi-filter-select') ? 'active' : '' !!}">Column Search</a></li>
+	<li><a href="{!! url('eloquent/add-edit-remove-column') !!}" class="{!! Request::is('eloquent/add-edit-remove-column') ? 'active' : '' !!}">Add/Edit/Remove Column</a></li>
+	<li><a href="{!! url('eloquent/dt-row') !!}" class="{!! Request::is('eloquent/dt-row') ? 'active' : '' !!}">DT Row Option</a></li>
+	<li><a href="{!! url('eloquent/custom-filter') !!}" class="{!! Request::is('eloquent/custom-filter') ? 'active' : '' !!}">Custom Filter</a></li>
+	<li><a href="{!! url('eloquent/carbon') !!}" class="{!! Request::is('eloquent/carbon') ? 'active' : '' !!}">DateTime/Carbon Objects</a></li>
+	<li><a href="{!! url('eloquent/relationships') !!}" class="{!! Request::is('eloquent/relationships') ? 'active' : '' !!}">Eager Loading</a></li>
+	<li><a href="{!! url('eloquent/joins') !!}" class="{!! Request::is('eloquent/joins') ? 'active' : '' !!}">Join Queries</a></li>
+</ul>
 
-<div class="list-group">
-	<a href="#" class="list-group-item disabled">Fluent/Query Builder</a>
-	<a href="{!! url('fluent/basic') !!}" class="list-group-item {!! Request::is('fluent/basic') ? 'active' : '' !!}">Basic</a>
-	<a href="{!! url('fluent/basic-object') !!}" class="list-group-item {!! Request::is('fluent/basic-object') ? 'active' : '' !!}">Object Data Source</a>
-    <a href="{!! url('fluent/multi-filter-select') !!}" class="list-group-item {!! Request::is('fluent/multi-filter-select') ? 'active' : '' !!}">Column Search</a>
-	<a href="{!! url('fluent/add-edit-remove-column') !!}" class="list-group-item {!! Request::is('fluent/add-edit-remove-column') ? 'active' : '' !!}">Add/Edit/Remove Column</a>
-	<a href="{!! url('fluent/dt-row') !!}" class="list-group-item {!! Request::is('fluent/dt-row') ? 'active' : '' !!}">DT Row Option</a>
-	<a href="{!! url('fluent/custom-filter') !!}" class="list-group-item {!! Request::is('fluent/custom-filter') ? 'active' : '' !!}">Custom Filter</a>
-    <a href="{!! url('fluent/carbon') !!}" class="list-group-item {!! Request::is('fluent/carbon') ? 'active' : '' !!}">DateTime/Carbon Objects</a>
-    <a href="{!! url('fluent/joins') !!}" class="list-group-item {!! Request::is('fluent/joins') ? 'active' : '' !!}">Join Queries</a>
-</div>
+<ul class="nav navbar-list">
+	<li class="header">Fluent/Query Builder</li>
+	<li><a href="{!! url('fluent/basic') !!}" class="{!! Request::is('fluent/basic') ? 'active' : '' !!}">Basic</a></li>
+	<li><a href="{!! url('fluent/basic-object') !!}" class="{!! Request::is('fluent/basic-object') ? 'active' : '' !!}">Object Data Source</a></li>
+    <li><a href="{!! url('fluent/multi-filter-select') !!}" class="{!! Request::is('fluent/multi-filter-select') ? 'active' : '' !!}">Column Search</a></li>
+	<li><a href="{!! url('fluent/add-edit-remove-column') !!}" class="{!! Request::is('fluent/add-edit-remove-column') ? 'active' : '' !!}">Add/Edit/Remove Column</a></li>
+	<li><a href="{!! url('fluent/dt-row') !!}" class="{!! Request::is('fluent/dt-row') ? 'active' : '' !!}">DT Row Option</a></li>
+	<li><a href="{!! url('fluent/custom-filter') !!}" class="{!! Request::is('fluent/custom-filter') ? 'active' : '' !!}">Custom Filter</a></li>
+    <li><a href="{!! url('fluent/carbon') !!}" class="{!! Request::is('fluent/carbon') ? 'active' : '' !!}">DateTime/Carbon Objects</a></li>
+    <li><a href="{!! url('fluent/joins') !!}" class="{!! Request::is('fluent/joins') ? 'active' : '' !!}">Join Queries</a></li>
+</ul>
 
-<div class="list-group">
-    <a href="#" class="list-group-item disabled">Collection [v5.x]</a>
-    <a href="{!! url('collection/basic') !!}" class="list-group-item {!! Request::is('collection/basic') ? 'active' : '' !!}">Basic</a>
-    <a href="{!! url('collection/basic-object') !!}" class="list-group-item {!! Request::is('collection/basic-object') ? 'active' : '' !!}">Object Data Source</a>
-    <a href="{!! url('collection/multi-filter-select') !!}" class="list-group-item {!! Request::is('collection/multi-filter-select') ? 'active' : '' !!}">Column Search</a>
-    <a href="{!! url('collection/array') !!}" class="list-group-item {!! Request::is('collection/array') ? 'active' : '' !!}">Array Data Source</a>
-    <a href="{!! url('collection/add-edit-remove-column') !!}" class="list-group-item {!! Request::is('collection/add-edit-remove-column') ? 'active' : '' !!}">Add/Edit/Remove Column</a>
-    <a href="{!! url('collection/dt-row') !!}" class="list-group-item {!! Request::is('collection/dt-row') ? 'active' : '' !!}">DT Row Option</a>
-    <a href="{!! url('collection/custom-filter') !!}" class="list-group-item {!! Request::is('collection/custom-filter') ? 'active' : '' !!}">Custom Filter</a>
-    <a href="{!! url('collection/carbon') !!}" class="list-group-item {!! Request::is('collection/carbon') ? 'active' : '' !!}">DateTime/Carbon Objects</a>
-</div>
+<ul class="nav navbar-list">
+    <li class="header">Collection [v5.x]</li>
+    <li><a href="{!! url('collection/basic') !!}" class="{!! Request::is('collection/basic') ? 'active' : '' !!}">Basic</a></li>
+    <li><a href="{!! url('collection/basic-object') !!}" class="{!! Request::is('collection/basic-object') ? 'active' : '' !!}">Object Data Source</a></li>
+    <li><a href="{!! url('collection/multi-filter-select') !!}" class="{!! Request::is('collection/multi-filter-select') ? 'active' : '' !!}">Column Search</a></li>
+    <li><a href="{!! url('collection/array') !!}" class="{!! Request::is('collection/array') ? 'active' : '' !!}">Array Data Source</a></li>
+    <li><a href="{!! url('collection/add-edit-remove-column') !!}" class="{!! Request::is('collection/add-edit-remove-column') ? 'active' : '' !!}">Add/Edit/Remove Column</a></li>
+    <li><a href="{!! url('collection/dt-row') !!}" class="{!! Request::is('collection/dt-row') ? 'active' : '' !!}">DT Row Option</a></li>
+    <li><a href="{!! url('collection/custom-filter') !!}" class="{!! Request::is('collection/custom-filter') ? 'active' : '' !!}">Custom Filter</a></li>
+    <li><a href="{!! url('collection/carbon') !!}" class="{!! Request::is('collection/carbon') ? 'active' : '' !!}">DateTime/Carbon Objects</a></li>
+</ul>
