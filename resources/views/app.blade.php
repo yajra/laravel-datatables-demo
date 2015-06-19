22ca28a55686c6dd9d3f6ff59bc2edbc362c6e00
Arjay Angeles <aqangeles@gmail.com>
2015-06-19 18:37:10 +0800

    donations :)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -87,7 +87,7 @@
 			<div class="col-sm-3 main">
 				<div class="panel panel-default text-center">
 					<div class="panel-heading">
-						<h3 class="panel-title">Donation Appreciated</h3>
+						<h3 class="panel-title">Donations Appreciated</h3>
 					</div>
 					<div class="panel-body">
 						<p>
