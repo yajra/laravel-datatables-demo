5f628205ce9215141c836a09304901ce363e1ac3
Arjay Angeles <aqangeles@gmail.com>
2015-09-22 16:42:45 +0800

    fix buttons assets url

--- resources/views/service/view.php
+++ resources/views/service/view.php
@@ -5,8 +5,8 @@
 @endsection
 
 @push('scripts')
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.9/css/jquery.dataTables.min.css">
-<script src="https://cdn.datatables.net/1.10.9/js/jquery.dataTables.min.js"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.0.3/css/buttons.dataTables.min.css">
+<script src="https://cdn.datatables.net/buttons/1.0.3/js/dataTables.buttons.min.js"></script>
 <script src="/vendor/datatables/buttons.server-side.js"></script>
 {!! $dataTable->scripts() !!}
 @stop
