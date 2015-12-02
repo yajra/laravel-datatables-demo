473bacb34e0491b40cb25d5e3cd02953889cc693
Arjay Angeles <aqangeles@gmail.com>
2015-12-02 14:29:33 +0800

    Revert DB prefix.

--- config/database.php
+++ config/database.php
@@ -60,7 +60,7 @@ return [
 			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
-			'prefix'    => 'demo_',
+			'prefix'    => '',
 			'strict'    => false,
 		],
 
