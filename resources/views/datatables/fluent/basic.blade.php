1cdeb7c335dd54a9e6d87f892a42f499108d4992
Arjay Angeles <aqangeles@gmail.com>
2016-02-20 11:10:40 +0800

    Update packages and add notes for basic demo.

--- resources/views/datatables/fluent/basic.blade.php
+++ resources/views/datatables/fluent/basic.blade.php
@@ -2,6 +2,15 @@
 
 @section('demo')
 <table id="users-table" class="table table-condensed">
+    <caption class="alert alert-success">
+        <p>
+            <strong>IMPORTANT: </strong>When using <strong>make()</strong>, the package will rely filtering and sorting based on the index/arrangement of your select query.
+        </p>
+        <br>
+        <p>
+            <strong>NEVER USE SELECT(*)</strong> when using this approach or your DataTables filtering/sorting may not work properly.
+        </p>
+    </caption>
     <thead>
         <tr>
             <th>Id</th>
