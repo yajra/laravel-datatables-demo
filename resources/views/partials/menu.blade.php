d5bba669ac0dc3275f37ffb9877298b58fc96e0f
Arjay Angeles <aqangeles@gmail.com>
2016-03-02 23:40:56 +0800

    Remove home menu.

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,5 +1,3 @@
-<li><a href="{{ url('/') }}">Home</a></li>
-
 <li class="dropdown">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Tutorials<span class="caret"></span></a>
     <ul class="dropdown-menu">
